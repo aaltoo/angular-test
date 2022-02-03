cce67ab5c838643557e5de87489fc3c963625388
y2k01 <scotlandfjord@protonmail.com>
2022-02-04 00:02:29 +0900

    Setting darkMode variable to localStorage

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
+import { Actions } from '@ngrx/effects';
 @Injectable()
 export class AppEffects {
   constructor(private actions$: Actions) {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { darkModeSelector } from './../../reducers/dark-mode';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { toggle } from 'src/app/state/actions/dark-mode.actions';
+import { async } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,12 +11,23 @@ import { toggle } from 'src/app/state/actions/dark-mode.actions';
 export class HeaderComponent implements OnInit {
   authorized = true;
   darkMode$ = this.store.select(darkModeSelector);
+  darkModeAsync: boolean | undefined;
+  darkModeFromLocalStorage: string | null = null;
 
   constructor(private store: Store) {}
 
   toggleDarkMode() {
     this.store.dispatch(toggle());
+    this.darkMode$.subscribe((data) => (this.darkModeAsync = data));
+    localStorage.setItem('darkMode', String(this.darkModeAsync));
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.darkModeFromLocalStorage = localStorage.getItem('darkMode');
+    if (this.darkModeFromLocalStorage === 'true') {
+      this.store.dispatch(toggle());
+    }
+  }
+
+  ngOnDestroy(): void {}
 }
